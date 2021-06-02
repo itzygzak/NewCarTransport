7b262821cf4c69734966ab0b150029baf73f27bc
Przemyslaw Duniec <pduniec@outlook.com>
2021-06-02 18:12:59 +0200

    Modify unit

--- Unit1.pas
+++ Unit1.pas
@@ -5,11 +5,15 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.MultiView;
+  FMX.Controls.Presentation, FMX.MultiView, FMX.TabControl;
 
 type
   TForm1 = class(TForm)
     mltvw1: TMultiView;
+    tbc1: TTabControl;
+    tbtm1: TTabItem;
+    tbtm2: TTabItem;
+    tbtm3: TTabItem;
   private
     { Private declarations }
   public
